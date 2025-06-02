d5c7ccb44e5a699c21c5026de253324209807bfc
SujalAgre <sujalagre26@gmail.com>
2025-06-02 20:54:31 +0530

    Blurt, a mobile soundboard app

--- App.js
+++ App.js
@@ -1,11 +1,43 @@
 import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
+import { StyleSheet, Text, View, TouchableOpacity } from 'react-native';
+import { useAudioPlayer } from 'expo-audio';
+import sounds from './sounds.js';
+import { useState } from 'react';
+
+import Darkmode from './assets/icons/dark-mode.svg'
+import Lightmode from './assets/icons/light-mode.svg'
 
 export default function App() {
+  const player = useAudioPlayer();
+  const [isDarkMode, setIsDarkMode] = useState(false);
+
+  const toggleTheme = () => {
+    setIsDarkMode(!isDarkMode);
+  }
+
+  const playSound = (index) => {
+    player.replace(sounds[index].sound);
+    player.seekTo(0);
+    player.play();
+  }
+
   return (
-    <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
-      <StatusBar style="auto" />
+    <View style={[styles.container, {backgroundColor: isDarkMode ? '#171717' : '#fff'}]}>
+      <StatusBar
+        style={isDarkMode ? "light" : "dark"}
+        backgroundColor="transparent"
+        translucent
+      />
+      <TouchableOpacity style={styles.themeButton} onPress={toggleTheme}>
+        {isDarkMode ? <Darkmode height={40} width={40} /> : <Lightmode height={40} width={40} />}
+      </TouchableOpacity>
+      <View style={styles.buttonContainer}>
+        {sounds.map((sound, index) => (
+          <TouchableOpacity key={index} onPress={() => playSound(index)} style={styles.button}>
+            <Text style={styles.buttonText}>{sound.emoji}</Text>
+          </TouchableOpacity>
+        ))}
+      </View>
     </View>
   );
 }
@@ -14,7 +46,31 @@ const styles = StyleSheet.create({
   container: {
     flex: 1,
     backgroundColor: '#fff',
-    alignItems: 'center',
     justifyContent: 'center',
   },
+  themeButton: {
+    position: 'absolute',
+    left: 20,
+    top: 50,
+    zIndex: 1000,
+  },
+  buttonContainer: {
+    flexDirection: 'row',
+    flexWrap: 'wrap',
+    justifyContent: 'center',
+    margin: 10,
+  },
+
+  button: {
+    padding: 10,
+    margin: 10,
+    borderRadius: 20,
+    borderWidth: 4,
+    borderColor: '#595959',
+    boxShadow: '3px 3px 0px 0px rgb(88, 88, 88)',
+    backgroundColor: '#fff',
+  },
+  buttonText: {
+    fontSize: 30,
+  }
 });
---
+++ sounds.js
@@ -0,0 +1,29 @@
+const sounds = [
+    {
+        emoji: '💥',
+        name: 'vine boom',
+        sound: require('./assets/sounds/vine-boom.mp3'),
+    },
+    {
+        emoji: '🔔',
+        name: 'undertakers-bell',
+        sound: require('./assets/sounds/undertakers-bell.mp3'),
+    },
+    {
+        emoji: '🔴',
+        name: 'among-us-sus',
+        sound: require('./assets/sounds/among-us-sus.mp3'),
+    },
+    {
+        emoji: '🕺',
+        name: 'chapri-music',
+        sound: require('./assets/sounds/chapri-music.mp3'),
+    },
+    {
+        emoji: '🎤',
+        name: 'let-me-know',
+        sound: require('./assets/sounds/let-me-know.mp3'),
+    },
+]
+
+export default sounds;
